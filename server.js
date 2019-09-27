212525637e55f108090df15a2a2d3e1238b16ead
ayiangio <moh.harisangio@gmail.com>
2019-09-27 13:01:14 +0700

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyPraser = require('body-parser')
+const user = require('./src/routes/user')
 app.use(
   bodyPraser.urlencoded({
     extended: true
@@ -10,3 +11,4 @@ app.use(
 app.use(bodyPraser.json())
 app.listen(port)
 console.log('Connect Succes On ' + port)
+app.use('/user', user)
